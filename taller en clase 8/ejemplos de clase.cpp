efd2df5cfdb940a8b46dc5c240efdcbd4d67fae3
juliandavidruizpardo <90577000+juliandavidruizpardo@users.noreply.github.com>
2021-09-15 21:32:36 -0500

    Update ejemplos de clase.cpp

--- taller en clase 8/ejemplos de clase.cpp
+++ taller en clase 8/ejemplos de clase.cpp
@@ -1,11 +1,26 @@
 /*T07E01::ESCRIBIR UN PROGRAMA EN VISUAL C++, QUE DESPLIEGUE LOS RESULTADOS
 DE SIGUIENTES EXPRESIONES: EJERCICIO DE CLASE
+si se conoce la distancia recorrida por un vehiculo (en kms) y el tiempo de
+desplazamiento (en horas), calcular la velocidad (en km/hora) y determinar si
+es o no infractor, sabiendo que el limite permitido de velocidad es de 80km/hora).
 NOMBRE: JULIAN DAVID RUIZ PARDO, VALENTINA MONTAÑA, SANTIAGO VARGAS
 ASIGNATURA:PROGRAMACION l*/
 #include <iostream>
 using namespace std;
-int
-main()
+int main()
 {
+	float distancia, tiempo, velocidad;
+	cout<<"distancia (km): ";	cin>>distancia;
+	cout<<"tiempo (h): ";		cin>>tiempo;
+	velocidad = distancia / tiempo;
+	cout<<"velocidad (km/h): "<<velocidad<<endl;
+	if(velocidad> 80)
+	{
+		cout<<"es infractor"<<endl;
+	}
+	else
+	{
+		cout<<"cumple con los limites permitidos"<<endl;
+	}
 	return 0;
-  
+}
