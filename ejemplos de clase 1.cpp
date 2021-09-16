0c96561c981740eef9c75a52ebab4252e59b5f19
valentinamh18 <90808655+valentinamh18@users.noreply.github.com>
2021-09-15 22:18:19 -0500

    Update ejemplos de clase 1.cpp

--- ejemplos de clase 1.cpp
+++ ejemplos de clase 1.cpp
@@ -1,11 +1,25 @@
-/*T07E01::ESCRIBIR UN PROGRAMA EN VISUAL C++, QUE DESPLIEGUE LOS RESULTADOS
-DE SIGUIENTES EXPRESIONES: EJERCICIO DE CLASE
-
-NOMBRE: JULIAN DAVID RUIZ PARDO, VALENTINA MONTAÑA, SANTIAGO VARGAS
+/*T07E01::ESCRIBIR UN PROGRAMA EN VISUAL C++, QUE LEA UN NUMERO DE TRES 
+CIFRAS Y DETERMINE SI ES PALINDROMO (EL MISMO AL LEERLO AL REVES, EJEMPLO: 474)   */
+NOMBRE: JULIAN DAVID RUIZ PARDO, VALENTINA MONTAÃ‘A, SANTIAGO VARGAS
 ASIGNATURA:PROGRAMACION l*/
 # incluye  < iostream >
-usando el  espacio de nombres  std;
+using name space std;
 int  main()
 {
+	int numero, centena, unidad;
+	cout<<"Digite un numero de 1 digito: ";		cin>>numero;
+	if(centena == unidad)
+	{
+		cout<<"El numero palindromo"<<endl;
+	}
+	else
+	{
+		cout<<"El numero no es palindromo"<<endl;
+	}
+}
+else
+{
+	cout<<"Error, debe digitar un numero entre 100 y 999"<<endl;
+}
 	return  0;
 }
